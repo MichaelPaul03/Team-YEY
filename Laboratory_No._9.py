12f2895bb7ac0488f16d27a5feb74e24779b4c8e
MichaelPaul03 <98940317+MichaelPaul03@users.noreply.github.com>
2022-05-21 12:24:56 +0800

    Add files via upload

---
+++ Laboratory_No._9.py
@@ -0,0 +1,14 @@
+import pyodbc
+
+connection = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=C:\Users\micha\Documents\Database1.accdb;')
+
+ID = 11
+Inventors = 'Legaspi, Mart Joven C.'
+Invention = 'Lab9'
+Date = '5/22/2022'
+Description = "First check if your pycharm or python program have the pydobc installed in the Python Interpreter. Once it is installed, you can now use pycharm to connect and edit a MS Access database. Adding data with the use of <object>.execute('INSERT INTO <TableName> (<ColName1>,<ColName2>,...) VALUES (?,?...)', (Parameters)). The use of [] for ColNames in order for spaces to be recognized. And don't forget to add the <object>.commit() in order for the execute to work"
+
+record = connection.cursor()
+record.execute('INSERT INTO tblInventor (ID, Inventors, Invention, [Date of Invention], Description) VALUES (?,?,?,?,?)', (ID, Inventors, Invention, Date, Description))
+record.commit()
+print("Data is added")
